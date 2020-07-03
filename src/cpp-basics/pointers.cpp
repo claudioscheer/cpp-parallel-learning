26f47e353d23b916f79dff2b67d76b09233357ca
Claudio Scheer <claudioscheer@protonmail.com>
2020-07-02 21:35:33 -0300

    Add pointer example

---
+++ src/cpp-basics/pointers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <assert.h>
+
+int main()
+{
+    // Store a value.
+    int value = 1;
+    std::cout << value << std::endl;
+
+    /*
+        & gets the pointer to a variable.
+        *value_address says that the variable stores a pointer.
+    */
+    int *value_address = &value;
+    std::cout << value_address << std::endl;
+
+    // Retrieve the value of a pointer.
+    int retrieved_value = *value_address;
+    std::cout << retrieved_value << std::endl;
+
+    assert(value == retrieved_value);
+
+    return 0;
+}
