ecaee98b9c727a324d5395d4a18c15bcc5457b2b
Claudio Scheer <claudioscheer@protonmail.com>
2020-07-02 21:02:51 -0300

    Refactor cpp-basics

--- src/cpp-basics/input.cpp
+++ src/cpp-basics/input.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout << "Enter your name: ";
-    string name;
-    getline(cin, name);
-    cout << "Your name is: " << name << endl;
+    std::cout << "Enter your name: ";
+    std::string name;
+    getline(std::cin, name);
+    std::cout << "Your name is: " << name << std::endl;
     int age;
-    cout << "Enter your age: ";
-    cin >> age;
-    cout << "Your age is: " << age << endl;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    std::cout << "Your age is: " << age << std::endl;
 
     return 0;
 }
