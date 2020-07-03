b848bac0c8d90d62e7caf565e1dd4de1836fd854
Claudio Scheer <claudioscheer@protonmail.com>
2020-07-03 01:05:57 -0300

    Add pthread_create example

--- src/pthread/create_threads.cpp
+++ src/pthread/create_threads.cpp
@@ -3,12 +3,29 @@
 
 #define THREADS_NUMBER 3
 
+void *thread_function(void *args)
+{
+    int pthread_id = (intptr_t)args;
+    std::cout << pthread_id << std::endl;
+    pthread_exit(NULL);
+}
+
 int main()
 {
     pthread_t threads[THREADS_NUMBER];
     for (int i = 0; i < THREADS_NUMBER; i++)
     {
         std::cout << "Creating thread " << i << "..." << std::endl;
+
+        // https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-constants
+        // https://stackoverflow.com/a/39950673
+        int err = pthread_create(&threads[i], NULL, &thread_function, (void *)(intptr_t)i);
     }
+
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
     return 0;
 }
