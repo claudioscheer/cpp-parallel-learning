28a64df8c2984f86d3e71f853a7736d044b4adb6
Claudio Scheer <claudioscheer@protonmail.com>
2020-07-23 14:21:27 -0300

    Example of class

---
+++ src/cpp-basics/class.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "class.h"
+
+double Operations::operate()
+{
+    return this->a * this->b;
+}
+
+int main()
+{
+    Operations *operations = new Operations();
+    operations->set_values(2.5, 1.0);
+
+    double result = operations->operate();
+
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
---
+++ src/cpp-basics/class.h
@@ -0,0 +1,15 @@
+class Operations
+{
+private:
+    double a;
+    double b;
+
+public:
+    void set_values(double a, double b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    double operate();
+};
